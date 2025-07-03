7efe61c22525de66b358cb55eca38ef8811bbe62
Styl6559 <shauryaiiitd@gmail.com>
2025-07-04 00:32:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { UiProvider } from './contexts/UiContext';
 import { AuthProvider } from './contexts/AuthContext';
 import ConfirmDialog from './components/ui/ConfirmDialog';
 import { HelmetProvider } from 'react-helmet-async';
+import DevLock, { isDevAccessGranted } from './components/DevLock';
 
 function App() {
+  const [unlocked, setUnlocked] = useState(() => isDevAccessGranted());
+
+  if (!unlocked) {
+    return <DevLock onUnlock={() => setUnlocked(true)} />;
+  }
+
   return (
     <HelmetProvider>
       <AuthProvider>
