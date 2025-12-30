f51ae30406fefb0ad11220d4c90ec5f068ba3a4f
Shaurya Tyagi <shauryaiiitd@gmail.com>
2025-12-31 01:48:54 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,6 @@ import { FundingProvider } from './contexts/FundingContext';
 import ConfirmDialog from './components/ui/ConfirmDialog';
 import { HelmetProvider } from 'react-helmet-async';
 
-function App() {
-  return (
-    <HelmetProvider>
-      <AuthProvider>
-        <FundingProvider>
-          <UiProvider>
-            <div className="min-h-screen bg-gray-900">
-              <RouterProvider router={router} />
-              <ConfirmDialog />
-            </div>
-          </UiProvider>
-        </FundingProvider>
-      </AuthProvider>
-    </HelmetProvider>
-  );
-}
-
-export default App;import React, { useState } from 'react';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes';
-import { UiProvider } from './contexts/UiContext';
-import { AuthProvider } from './contexts/AuthContext';
-import { FundingProvider } from './contexts/FundingContext';
-import ConfirmDialog from './components/ui/ConfirmDialog';
-import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
